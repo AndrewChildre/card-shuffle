d80442d2593660d838e90b5d5a442ab8169b01cc
andrew childre <andrewchildre@gmail.com>
2021-05-22 16:37:04 -0600

    start-menu-style

--- src/App.js
+++ src/App.js
@@ -2,11 +2,12 @@ import './App.css';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
 import Deck from './Components/Deck'
 import DeckDisplay from './Components/DeckDisplay'
-
+import Nav from './Components/Nav'
 
 function App() {
   return (
     <div className="App">
+      <Nav />
   <Deck/>
    
     </div>
