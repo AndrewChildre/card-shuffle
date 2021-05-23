069f0aefe5f54dc8ca8713a8e361337fab16b2ed
andrew childre <andrewchildre@gmail.com>
2021-05-23 01:12:56 -0600

    working proto

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import './App.css';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
 import Deck from './Components/Deck'
-import DeckDisplay from './Components/DeckDisplay'
+
 import Nav from './Components/Nav'
 
 function App() {
